ce39739eb7ec2123b69466dbd92b187774b4d6e5
Berk Certel <m.berkcertel@outlook.com>
2026-05-08 23:33:34 +0300

    project created

---
+++ styles/auth.styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 50,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+});
